abadbebe421ca91bde59e60c1bd760d0432975be
Brenda Sánchez Reyes <sanrey254@hotmail.com>
2018-10-17 17:43:33 -0500

    Importando modelos al servidor

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const { ApolloServer, gql } = require("apollo-server");
 const mongoose = require("mongoose");
 require("dotenv").config({ path: "variables.env" });
+const fs = require("fs");
+const path = require("path");
+
+const Drink = require("./models/Drink");
+const Food = require("./models/Food");
+const Order = require("./models/Order");
+const User = require("./models/User");
+
+const filePath = path.join(__dirname, "typeDefs.gql");
+const typeDefs = fs.readFileSync(filePath, "utf-8");
 
 mongoose
   .connect(
@@ -10,18 +20,15 @@ mongoose
   .then(() => console.log("Base de datos conectada"))
   .catch(error => console.log(error));
 
-const typeDefs = gql`
-  type Lista {
-    tarea: String
-    persona: String
+const server = new ApolloServer({
+  typeDefs,
+  context: {
+    Drink,
+    Food,
+    User,
+    Order
   }
-
-  type Query {
-    getLista: [Lista]
-  }
-`;
-
-const server = new ApolloServer({ typeDefs });
+});
 
 server.listen().then(({ url }) => {
   console.log("Servidor activo ", url);
