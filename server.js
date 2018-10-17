913d02442c93b6f1bad5b2dd049a7362d9203e0e
Brenda Sánchez Reyes <sanrey254@hotmail.com>
2018-10-17 13:44:29 -0500

    Creando servidor

--- server.js
+++ server.js
@@ -1,1 +1,44 @@
-console.log("hola");
+const { ApolloServer, gql } = require("apollo-server");
+
+const lista = [
+  { tarea: "leer", persona: "Adri" },
+  { tarea: "comer", persona: "Copelia" },
+  { tarea: "caminar", persona: "Rocio" },
+  { tarea: "saltar", persona: "Linda" }
+];
+
+const typeDefs = gql`
+  type Lista {
+    tarea: String
+    persona: String
+  }
+
+  type Query {
+    getLista: [Lista]
+  }
+
+  type Mutation {
+    addLista(tarea: String, persona: String): Lista
+  }
+`;
+
+const resolvers = {
+  Query: {
+    getLista: () => {
+      return lista;
+    }
+  },
+  Mutation: {
+    addLista: (_, args) => {
+      const nuevalista = { tarea: args.tarea, persona: args.persona };
+      lista.push(nuevalista);
+      return nuevalista;
+    }
+  }
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log("Servidor activo ", url);
+});
