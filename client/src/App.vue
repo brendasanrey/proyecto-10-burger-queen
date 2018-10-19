d62a84cce24986bbb4fac6dacfdb7b57ecd0f831
Brenda Sánchez Reyes <sanrey254@hotmail.com>
2018-10-19 12:41:44 -0500

    Agregando logo y boton de ingreso

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,47 +1,21 @@
 <template>
   <div id="app">
-
-    <!-- Barra de navegación -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-blue">
-      <a class="navbar-brand" href="/">Burger Queen</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item active" v-for="item in itemsOfNav" :key="item.title" :to="item.link">
-            <a class="nav-link" :href="item.link">
-              <i :class="item.class"></i> {{item.title}}</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-
     <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: "App",
-  computed: {
-    itemsOfNav() {
-      return [
-        { title: "Ingresar", link: "/signin", class: "fas fa-sign-out-alt" },
-        { title: "Registrarse", link: "/signup", class: "fas fa-user-plus" }
-      ];
-    }
-  }
+  name: "App"
 };
 </script>
 
 <style>
-.bg-blue {
-  background-color: #25ccca;
-}
-.nav-item:hover {
-  background-color: #1ab5b3;
+.bg {
+  background-color: #f6f6f6;
 }
 </style>
+
+
+
 
--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,6 +1,15 @@
 <template>
-  <div class="container mt-3">
-    <h1>Home</h1>
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col-md-6 offset-md-3 col-12 text-center">
+        <img src="https://preview.ibb.co/kXjHgL/logo.jpg" class="img-fluid img-burger">
+      </div>
+      <div class="col-md-6 offset-md-3 col-12 text-center mt-3">
+        <button class="btn btn-danger">
+          <i class="fas fa-sign-in-alt"></i> Ingresar</button>
+      </div>
+
+    </div>
   </div>
 </template>
 
@@ -9,3 +18,10 @@ export default {
   name: "home"
 };
 </script>
+
+<style>
+.img-burger {
+  height: 400px;
+}
+</style>
+
